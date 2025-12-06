7265a8a3b4dc37ff30df05ce39c0e3f2893e9837
Daggerheart12 <danielunirider@outlook.com>
2025-12-06 17:02:18 +0930

    Commit 3
    Javascript file now reads JSON data and writes it to HTML.
    
    Cards can be made, but not deleted yet.
    
    The script will update the card data from the file on a five-second interval.

--- data_display/testing/script.js
+++ data_display/testing/script.js
@@ -1,12 +1,15 @@
-window.onload = function() {
-    //Define global constants.
-    const card_container = document.getElementById("card_container");
-    
+//Define constants.    
+const card_container = document.getElementById("card_container");
 
+//Set an interval to check the data file for new data.
+window.setInterval(updateCardContent, 5000);
+
+//Do stuff at the start.
+window.onload = function() {   
     //Not working? Something is wrong.
     focus(document.getElementById("1"));
     console.log(document.activeElement.id);
-    updateCards();
+    updateCardContent();
 
     //Start the input handling.
     window.addEventListener("keydown", handleInput);   
@@ -22,11 +25,10 @@ function handleInput(e) {
             nextCard();
             break;
         case "r":
-            const data = getData();
-            console.log(data);
-            break;
+            updateCardContent()            
+
         default:
-            console.log(e.key);
+            //console.log(e.key);
             break;
     }
 }
@@ -36,8 +38,7 @@ function previousCard() {
     const card = document.activeElement;
 
     if (card.previousElementSibling != null) {
-        console.log(card.previousElementSibling.id);
-        updateCards(document.getElementById(card.previousElementSibling.id));
+        updateCardFocus(card.previousElementSibling.id);
     }
 }
 
@@ -46,45 +47,65 @@ function nextCard() {
     const card = document.activeElement;
 
     if (card.nextElementSibling != null) {
-        console.log(card.nextElementSibling.id);
-        updateCards(document.getElementById(card.nextElementSibling.id));
+        updateCardFocus(card.nextElementSibling.id);
     }
 }
 
-//Loop through all of the cards in the list, reset them all, and then flip the card that was selected. 
-function updateCards(selected_card) {
-    var cards = card_container.getElementsByTagName('div');
-
-    for (i = 0; i < cards.length; i ++) {
-        var card = cards[i];
-
-        if (selected_card != null && card == selected_card) {
+//With a new card selected, flip the card, and update the rest to unflip the last one.
+async function updateCardFocus(next_card_id) {   
+    
+    if (document.getElementById(next_card_id) != null)
+        {
+            var card = document.getElementById(next_card_id);
+            replaceCardData(next_card_id, ["This card is focused", "The text has been replaced", "Dogs are the best"])
             card.tabIndex = 0;
-            card.children[0].innerHTML = "focused";
-            card.style.backgroundColor = "blue";
-            card.focus()
+            card.focus();
         }
-
         else {
-            card.tabIndex = -1;
-            card.children[0].innerHTML = "unfocused";
-            card.style.backgroundColor = "grey";
+            console.error("The card with the ID ${next_card_id} does not exist.")
         }
-    }
+        
+    //Refresh card data.
+    await updateCardContent()
+}
+
+//Loop through all of the cards in the list, reset them all, and then flip the card that was selected. 
+//This function will not overwrite data on focused cards.
+async function updateCardContent(selected_card) {
+
+     //Get cards.
+    var cards = card_container.getElementsByClassName('info_card');
+
+    //Get data from the file.
+    const data_path = "http://localhost:8080/data/data.json";
+    data = await getData(data_path);
+    
+
+    //Go through the data, and replace update all the cards in the list.
+    //This includes adding new cards, and removing redundant ones.
+    for (var i = 0; i < data.length; i++) {
+        if (cards[i] != null) {
+            if (cards[i] != document.activeElement) {
+                replaceCardData(i, data[i]);
+            }
+        }
+        else {
+            createNewCard(i);
+            replaceCardData(i, data[i]);
+        }
+    }    
 }
 
 //Read from the data file.
 async function getData(path) {
     //File path to data. This assumes that the server is hosting on port 8080, that could be a problem later.
-    const data_path = "http://localhost:8080/data/data.json";
-
     try {
-        const response = await fetch(data_path);
+        const response = await fetch(path);
         if (!response.ok) {
             throw new Error("${response.status}");
         }
 
-        return (await response.json());
+        return (await response.json());        
     }
     //Catch should only be called when an error is caught, and isn't called. It needs a variable to represent the error.
     catch (error) {
@@ -92,5 +113,45 @@ async function getData(path) {
     }
 }
 
+//Search for, and replace existing card data using a card ID and provided device data.
+function replaceCardData(card_id, data) {
+    //Get the card object from the document.
+    var card = document.getElementById(card_id);
+    
+    card.querySelector('p[id=name]').innerHTML = data[0];
+    card.querySelector('p[id=cpu_load]').innerHTML = data[1];
+    card.querySelector('p[id=cpu_temp]').innerHTML = data[2];
+}
+
+//Create a new card and populate it with the required elements.
+function createNewCard(card_id) {
+    //Create a new card element.
+    var new_info_card = document.createElement("div");
+
+    //Assign content to the new card.
+    new_info_card.className = "info_card";
+    new_info_card.id = card_id;
+    new_info_card.innerHTML = getInfoCardHtml();
+    
+    //Ensure that the starting card is focusable.
+    if (card_id == 0) {
+        new_info_card.tabIndex = '0';        
+    }
+    else {
+        new_info_card.tabIndex = '-1';
+    }
+
+    //Append the card to the container.
+    card_container.appendChild(new_info_card);
+}
+
+//Return the base card HTML for making new cards.
+function getInfoCardHtml(card_id) {
+    return `
+    <p id=name></p>
+    <p id=cpu_load></p>
+    <p id=cpu_temp></p>
+    `;
+}
 
 
