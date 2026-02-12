7b0a7cb6752a93170cbbaf31f7dbd84ed214fe66
sirsegv <sirsegv@sirsegv.moe>
2026-02-12 11:14:02 +1030

    Remove platform-specific logic from server start

--- server_system/main.py
+++ server_system/main.py
@@ -1,31 +1,15 @@
 
-import os
-from random import randint
-from time import sleep
-from platform import system
+from threading import Thread
 
 import receive_data
+import server_handler
 
+PORT=8080
 
-PORT = 8080
+Thread(target=server_handler.start_server, args=(PORT,)).start()
 
+receive_data.start_api_endpoint()
 
-
-#Initialise HTTP server.
-def start_server():
-    if system() == "Linux":
-        print("Running on Linux")
-        os.system("xterm & -e sudo python server_system/server_handler.py")
-
-    else:
-        os.system("start cmd /k python3 server_system/server_handler.py")
-
-#Initialise API endpoint.
-def start_api_endpoint():
-    receive_data.start_api_endpoint()
-
-start_server()
-start_api_endpoint()
 
 
 
